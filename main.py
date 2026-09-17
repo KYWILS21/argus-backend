5bbe7df54f749e8efed4de4ed2441cc90e5949fb
KYWILS21 <156925256+KYWILS21@users.noreply.github.com>
2026-09-17 12:38:08 -0400

    Additional changes to main and index.html

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "your_secret_access_token")
